3dee04237eb4eabc2a0d4575c337460af9970354
GEEDAAS <gerardoguerrerof21@gmail.com>
2025-05-20 05:24:39 -0600

    Delete

--- public/js/Cuenta.js
+++
@@ -1,9 +0,0 @@
-// Función para abrir un modal dado su ID
-function openModal(id) {
-    document.getElementById(id).style.display = 'block'; // Muestra el modal modificando su estilo
-}
-
-// Función para cerrar un modal dado su ID
-function closeModal(id) {
-    document.getElementById(id).style.display = 'none'; // Oculta el modal modificando su estilo
-}
--- public/js/Sesion_Admi.js
+++
@@ -1,5 +0,0 @@
-// Función que abre una nueva pestaña con la página de sesión del usuario
-function abrirPaginaUsuario() {
-  // Abre el archivo Sesion.html en una nueva pestaña del navegador
-  window.open('Sesion.html', '_blank');
-}
